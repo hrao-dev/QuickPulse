2f1c7e883e9fa6d60cf3ef67c3e6dd38f4208435
Harini R <harao-ml@users.noreply.huggingface.co>
2026-04-09 14:55:17 +0000

    Update summarizer.py

--- summarizer.py
+++ summarizer.py
@@ -4,8 +4,8 @@
 from transformers import pipeline
 
 # Load summarization pipeline
-#summarizer = pipeline("summarization", model="harao-ml/flant5-finetuned-summarize")
-summarizer = pipeline("text-generation", model="harao-ml/flant5-finetuned-summarize")
+summarizer = pipeline("summarization", model="harao-ml/flant5-finetuned-summarize")
+#summarizer = pipeline("text-generation", model="harao-ml/flant5-finetuned-summarize")
 
 # Function to split text into smaller chunks
 def split_text(text, max_tokens=512):
